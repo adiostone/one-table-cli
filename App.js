76716bebf330e26838ccb16c6191b1b15e05bdd6
wseungjin <bose1021@naver.com>
2020-04-24 00:46:15 +0900

    chore : install expo secure store

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import loginScreen from './src/screen/loginScreen'
 import partyScreen from './src/screen/partyScreen'
+import * as SecureStore from 'expo-secure-store';
 
 
 const Stack = createStackNavigator();
