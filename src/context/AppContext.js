fa33e50cf3c626548aeb5524a5032935265b797c
wseungjin <bose1021@naver.com>
2020-04-25 23:05:23 +0900

    chore : update appContext location features

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -6,6 +6,10 @@ export const AppContext = createContext()
 export const AppProvider =({children}) =>{
     const [accessToken, setAccessToken] = useState('')
     const [refreshToken, setRefreshToken] = useState('')
+    const [location, setLocation] = useState();  
+    const [formattedAddress, setFormattedAddress] = useState("");
+    const [detailLocation, setDetailLocation] = useState("");
+    const [locationIsSet, setLocationIsSet] = useState(false);
   
     return (
       <AppContext.Provider
