7b355dbb55d363b5b5c63e293445d4cd30e0cfe6
wseungjin <bose1021@naver.com>
2020-05-05 20:09:34 +0900

    feat: load information from your data

--- src/screen/mapScreen.js
+++ src/screen/mapScreen.js
@@ -6,91 +6,119 @@ import { Marker } from 'react-native-maps';
 import Geocoder from 'react-native-geocoding';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import { AppContext } from '../context/AppContext'
+import * as SecureStore from 'expo-secure-store';
+
+
 
 import config from "../config/config"
 
 export default function mapScreen({navigation}) {
 
-  const [textInput, onChangeText] = useState('세부주소를 입력해주세요');
-  const [location, setLocation] = useState();  
-  const [formattedAddress, setFormattedAddress] = useState("");
-  const [detailLocation, setDetailLocation] = useState("");
-  const [locationIsSet, setLocationIsSet] = useState(false);
-  const [mapRegion, setMapRegion] = useState();
-  const [errorMsg, setErrorMsg] = useState();
+  const appContext = useContext(AppContext)
 
-  Geocoder.init(config.googleAPIKey,{language: "korean"})
+  const [textInput, setTextInput] = useState(appContext.detailAddress);
+  const [location, setLocation] = useState(appContext.location);  
+  const [formattedAddress, setFormattedAddress] = useState(appContext.formattedAddress);
+  const [detailAddress, setDetailAddress] = useState(appContext.detailAddress);
+  const [locationIsSet, setLocationIsSet] = useState(appContext.locationIsSet);
+  const [mapRegion, setMapRegion] = useState(appContext.mapRegion);
 
   useEffect(() => {
-      if(locationIsSet===false){
-        getlocation();
+      if(locationIsSet==="false"){
+        console.log("getLocation")
+        getNewlocation();
     };
-  });
+  },[locationIsSet]);
 
-  const getlocation = async () => {
-    let { status } = await Location.requestPermissionsAsync();
+  function setDetailLocation(e) {
+    setDetailAddress(textInput)
+    appContext.setDetailAddress(textInput)
+    SecureStore.setItemAsync('detailAddress',textInput).then(res => {
+      console.log("setDetailAddress")
+    }
+    )
+  }
+
+  const getNewlocation = async () => {
+
+      Geocoder.init(config.googleAPIKey,{language: "korean"})
+      let { status } = await Location.requestPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        console.log('Permission to access location was denied');
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setMapRegion({ 
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
+      let googleLocation = await Location.getCurrentPositionAsync({});
+      let location = { 
+        latitude: googleLocation.coords.latitude,
+        longitude: googleLocation.coords.longitude,
+      }
+      let mapRegion = {
+        latitude: googleLocation.coords.latitude,
+        longitude: googleLocation.coords.longitude,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
-      });
+      }
+      setLocation(location)
+      appContext.setLocation(location);
+      setMapRegion(mapRegion);
+      appContext.setMapRegion(mapRegion);
       Geocoder.from({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude
+        latitude: googleLocation.coords.latitude,
+        longitude: googleLocation.coords.longitude
       }).then(json => {
             setFormattedAddress(json.results[0].formatted_address);
+            appContext.setFormattedAddress(json.results[0].formatted_address);
+            SecureStore.setItemAsync('formattedAddress',json.results[0].formatted_address)
         })
         .catch(error => console.warn(error));
-      setLocationIsSet(true);
+      appContext.setLocationIsSet("true");
+      setLocationIsSet("true");
+      appContext.setDetailAddress("세부정보를 입력해주세요");
+      setDetailAddress("세부정보를 입력해주세요");
+      setTextInput("세부정보를 입력하세요");
+      SecureStore.setItemAsync('location', JSON.stringify(location))
+      SecureStore.setItemAsync('mapRegion',JSON.stringify(mapRegion))
+      SecureStore.setItemAsync('locationIsSet',"true")
+
   };
-
-
-  let lat ='Waiting..';
-  let long = "waiting.."
-  let coord = {coordinate:{latitude: 0 ,longitude: 0}}
-  let address = "waiting.."
-  if (errorMsg) {
-    lat = errorMsg;
-    long = errorMsg
-  } else if (location) {
-    lat = location.coords.latitude;
-    long = location.coords.longitude;
-    coord = location.coords
-    address = formattedAddress 
-   }
 
   return (
     <SafeAreaView style={styles.container}>
         <Image source={require('../assets/OnetableLogo.png')} style={styles.logoStyle} />
         <ScrollView style={styles.mapContainer}>
+            {(locationIsSet==="true") ?
+            //main page
+            (<View>
             <View style={styles.resetLocationBox}>
               <Text style={styles.resetLocationBoxText}>주소</Text>
             </View>        
             <View style={styles.locationBox}>
-              <Text style={styles.locationBoxText}>{address}</Text>
+              <Text style={styles.locationBoxText}>{formattedAddress}</Text>
             </View>     
             <View style={styles.locationBox}>
-              <TextInput style={styles.locationBoxText} onPress={(text) => onChangeText(text)}>{textInput}</TextInput>
+              <TextInput style={styles.locationBoxText} onChangeText={(text) => setTextInput(text)}>{textInput}</TextInput>
             </View>     
-            <TouchableOpacity style={styles.resetLocationBox} onPress={() => setDetailLocation(textInput)}>
+            <TouchableOpacity style={styles.resetLocationBox} onPress={setDetailLocation}>
               <Text style={styles.resetLocationBoxText}>세부 주소 설정</Text>
             </TouchableOpacity>
             <MapView style={styles.mapStyle} region={mapRegion}>
-              <Marker coordinate={coord}/>
+              <Marker coordinate={location}/>
             </MapView>
-            <TouchableOpacity style={styles.resetLocationBox} onPress={() => setLocationIsSet(false)}>
+            <TouchableOpacity style={styles.resetLocationBox} onPress={() => setLocationIsSet("false")}>
             <Text style={styles.resetLocationBoxText}> 위치 재설정</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.backBox} onPress={() => navigation.navigate('main')}>
               <Text style={styles.backBoxText}> 돌아가기</Text>
             </TouchableOpacity>
+            </View>) : 
+            //loading page
+            (
+              <View style={styles.loadingContainer}>
+                <Text style={styles.loadingText}>로딩중</Text>
+              </View>
+            )
+            }
       </ScrollView>
     </SafeAreaView>
       
@@ -172,4 +200,16 @@ const styles = StyleSheet.create({
     marginTop:20,
     marginBottom:15,
   },
+  loadingContainer:{
+    height :  650,
+    justifyContent: 'center', 
+    alignItems: 'center' ,
+    alignSelf: 'center',
+
+  },
+  loadingText:{
+    fontSize: 50,
+    textAlign: "center",
+
+  }
 });
