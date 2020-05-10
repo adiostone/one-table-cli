ca9ccdeed9752e2e7bf68427ba15601b6e1c782a
wseungjin <bose1021@naver.com>
2020-05-10 14:33:05 +0900

    feat : userItem save everyUser Info for each party

---
+++ src/component/userItem.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { StyleSheet, Text, View,Button, Image,TextInput,Dimensions ,ScrollView,TouchableOpacity,SafeAreaView} from 'react-native';
+import { AppContext } from '../context/AppContext'
+import * as SecureStore from 'expo-secure-store';
+import axios from 'axios'
+
+export default function userItem(itemData) {
+
+  const [userID, setUserID] = useState(itemData.data.userID) 
+  const [nickname, setNickname] = useState(itemData.data.nickname) 
+
+  useEffect(()=>{
+   
+  })
+
+  return (
+
+          <View style={styles.listBox}>
+            <Text style={styles.foodNameText}> {nickname}</Text>
+          </View>        
+      
+  );
+}
+
+
+const styles = StyleSheet.create({
+
+  listBox: {
+    width: 335,
+    height: 39,
+    backgroundColor: "#FFF5F5",
+    borderRadius: 10,
+    marginBottom : 12, 
+    justifyContent: 'center', 
+    alignItems: 'center' ,
+    alignSelf: 'center',
+
+},
+foodNameText:{
+
+  fontStyle: 'normal',
+  fontSize: 14,
+  textAlign: "center",
+
+},
+
+});
---
+++ src/component/userList.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { StyleSheet, Text, View,Button, Image,TextInput,Dimensions ,ScrollView,TouchableOpacity,SafeAreaView} from 'react-native';
+import { AppContext } from '../context/AppContext'
+import * as SecureStore from 'expo-secure-store';
+import axios from 'axios'
+import UserItem from './userItem';
+
+
+export default function userList() {
+
+    const [userList, setUserList] = useState({
+        data: [
+            {nickname: "bose1021"},
+            {nickname: "dntmdmwl24024"},
+
+        ]
+    });
+
+    useEffect(() => {
+        // addDummyItem()
+    },[])
+
+
+    // function addDummyItem(){
+    //     setRestaurantList({
+    //         data: [
+    //             {name: "네네 치킨",deliveryFee: 2000 ,atLeastMoney: 15000},
+    //             {name: "비비큐", deliveryFee: 3000 ,atLeastMoney: 16000},
+    //             {name: "굽네 치킨", deliveryFee: 2000 ,atLeastMoney: 15000},
+    //             {name: "호식이 두마리", deliveryFee: 1000 ,atLeastMoney: 14000},
+    //         ]
+    //     });
+    // }
+
+  return (
+    <ScrollView style={styles.foodContainer}>
+        {userList.data.map((data,i) => {
+        return (<UserItem key={i} data={data}/>);
+        })}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  foodContainer: {
+    display : "flex",
+    flexDirection: "column",
+    alignContent : "stretch",
+  },
+
+});
