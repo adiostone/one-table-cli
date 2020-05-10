56a7fd883736f2c39c249a979ed2f093279e880a
wseungjin <bose1021@naver.com>
2020-05-10 14:30:24 +0900

    feat : make room Screen UI

--- src/screen/roomScreen.js
+++ src/screen/roomScreen.js
@@ -1,20 +1,41 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { StyleSheet, Text, View,Button, Image,TextInput,TouchableOpacity,Dimensions, ScrollView, SafeAreaView } from 'react-native';
-
+import { AppContext } from '../context/AppContext'
 import LogoButton from "../component/logoButton"
+import UserList from "../component/userList"
+
 
 export default function roomScreen({navigation}) {
+
+  const appContext = useContext(AppContext)
+
+  const [partyID, setPartyID] = useState() 
+  const [restaurantName, setRestaurantName] = useState() 
+  const [address, setAddress] = useState() 
+  const [capacity, setCapacity] = useState();
+  const [curPeopleNum, setCurPeopleNum] = useState();
+  const [partyName, setPartyName] = useState() 
+
+  const [userList, setUserList] = useState() 
+
 
     return (
         <SafeAreaView style={styles.container}>
           <LogoButton/>
           <ScrollView style={styles.pinContainer}>
+            <View style={styles.listBox} onPress={() => {navigation.navigate("room")}}>
+              <Text style={styles.restaurantNameText}> {restaurantName}</Text>
+              <Text style={styles.addressText}>{address}</Text>
+              <Text style={styles.addressText}>{address}</Text>
+              {/* <Text style={styles.peopleNumberText}> {curPeopleNum}/{capacity}</Text> */}
+            </View>        
             <TouchableOpacity style={styles.shoppingBagBox} onPress={() => navigation.navigate('shoppingBag')}>
               <Text style={styles.shoppingBagText}>장바구니</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.addMenuBox} onPress={() => navigation.navigate('foodList')}>
               <Text style={styles.addMenuText}>메뉴 추가</Text>
-            </TouchableOpacity>        
+            </TouchableOpacity>   
+            <UserList/>     
           </ScrollView>
         </SafeAreaView>
 
@@ -31,6 +52,38 @@ export default function roomScreen({navigation}) {
     pinContainer: {
       alignSelf: 'center',
   
+    },
+    listBox:{
+      width: 335,
+      height: 80,
+  
+      backgroundColor: "#CB661D",
+      borderRadius: 10,
+      marginBottom : 12, 
+      justifyContent: 'center', 
+      alignItems: 'center' 
+  
+    },
+    restaurantNameText:{
+      fontStyle: 'normal',
+      fontSize: 18,
+      fontWeight : "bold",
+      color: "#FFFFFF",
+    },
+    addressText:{
+      fontStyle: 'normal',
+      fontSize: 15,
+      color: "#FFFFFF",
+    },
+    partyNameText:{
+      fontStyle: 'normal',
+      fontSize: 12,
+      color: "#FFFFFF",
+    },
+    peopleNumberText:{
+      fontStyle: 'normal',
+      fontSize: 12,
+      color: "#FFFFFF",
     },
     addMenuBox:{
       width: 335,
