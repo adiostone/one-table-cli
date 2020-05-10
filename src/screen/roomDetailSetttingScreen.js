cb7d8780f4cf5edd2ea7916c6b87be9786e0c367
wseungjin <bose1021@naver.com>
2020-05-10 14:31:20 +0900

    feat : add roomDetailSetting UI + get info

--- src/screen/roomDetailSetttingScreen.js
+++ src/screen/roomDetailSetttingScreen.js
@@ -1,34 +1,42 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { StyleSheet, Text,SafeAreaView , TouchableOpacity,View,Button, Image,TextInput,Dimensions } from 'react-native';
 import LogoButton from "../component/logoButton"
+import { AppContext } from '../context/AppContext'
 
-export default function roomDetailSettingScreen({navigation}) {
+
+export default function roomDetailSettingScreen({route, navigation}) {
+
+  const appContext = useContext(AppContext)
+
+  const [restaurantID, setRestaurantID] = useState(route.params.restaurantID);
+  const [restaurantName, setRestaurantName] = useState(route.params.restaurantName);
+
+  const [roomName, setRoomName] = useState("같이 먹어요");
+  const [capacity, setCapacity] = useState(3);
+
+  const [formattedAddress, setFormattedAddress] = useState(appContext.formattedAddress);
+  const [detailAddress, setDetailAddress] = useState(appContext.detailAddress);
 
     return (
       <SafeAreaView style={styles.container}>
           <LogoButton/>
           <View style={styles.roomDetailContainer}>
               <View style={styles.restaurantNameBox}>
-                <Text style={styles.restaurantNameText}>네네치킨 중앙대점</Text>
+              <Text style={styles.restaurantNameText}>{restaurantName}</Text>
               </View>
               <View style={styles.detailSettingBox}>
                 <Text style={styles.detailSettingText}>방제목</Text>                  
               </View>
-              <View style={styles.detailSettingInputBox}>
-                  <TextInput style={styles.detailSettingInputText}></TextInput>
-              </View>
+              <TextInput style={styles.detailSettingInputBox} onChangeText={(text) => setRoomName(text)}>{roomName}</TextInput>
               <View style={styles.detailSettingBox}>
                 <Text style={styles.detailSettingText}>최대인원</Text>                  
               </View>
-              <View style={styles.detailSettingInputBox}>
-                  <TextInput style={styles.detailSettingInputText}></TextInput>
-              </View>
+              <TextInput style={styles.detailSettingInputBox} onChangeText={(text) => setCapacity(text)}>{capacity}</TextInput>
               <View style={styles.detailSettingBox}>
                 <Text style={styles.detailSettingText}>만나는 위치</Text>                  
               </View>
-              <View style={styles.detailSettingInputBox}>
-                  <TextInput style={styles.detailSettingInputText}></TextInput>
-              </View>
+              <TextInput style={styles.detailSettingInputBox} onChangeText={(text) => setFormattedAddress(text)}>{formattedAddress}</TextInput>
+              <TextInput style={styles.detailSettingInputBox} onChangeText={(text) => setDetailAddress(text)}>{detailAddress}</TextInput>
               <TouchableOpacity style={styles.buttonBox} onPress={() => navigation.navigate('room')}>
                 <Text style={styles.buttonText}>파티 만들기</Text>
               </TouchableOpacity>
@@ -98,9 +106,6 @@ export default function roomDetailSettingScreen({navigation}) {
       justifyContent: 'center', 
       alignItems: 'center' ,
       alignSelf: 'center',
-
-    },
-    detailSettingInputText:{
       fontStyle: 'normal',
       fontSize: 20,
       textAlign: "center",
