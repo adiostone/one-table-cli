aa878f4a672cf2a273347cb013094a465b88010b
wseungjin <bose1021@naver.com>
2020-05-18 02:49:49 +0900

    feat : new UI for restarantItem and apply icon

--- src/component/restaurantItem.js
+++ src/component/restaurantItem.js
@@ -10,9 +10,10 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function restaurantItem(props) {
 
-    const restaurantID =props.data.id 
+    const id =props.data.id 
+    const icon =props.data.icon 
     const category =props.data.category 
-    const restaurantName = props.data.name 
+    const name = props.data.name 
     const deliveryFee =props.data.deliveryFee 
     const minOrderPrice =props.data.minOrderPrice 
 
@@ -21,11 +22,16 @@ export default function restaurantItem(props) {
   return (
 
         <TouchableOpacity style={styles.restaurantButton}  onPress={() => {navigation.navigate("roomDetailSetting",{
-          restaurantID: restaurantID,
-          restaurantName: restaurantName,
+          id: id,
+          name: name,
         })}}>
-        <Text style={styles.restaurantNameText}>{restaurantName}</Text>
-        <Text style={styles.restaurantDetailText}>최소주문금액 {minOrderPrice}원</Text>
+          <View style={styles.leftBox}>
+            <Image source={{uri:icon}} style={styles.imageStyle}/>
+          </View>
+          <View style={styles.rightBox}>
+            <Text style={styles.restaurantNameText}>{name}</Text>
+            <Text style={styles.restaurantDetailText}>최소주문금액 {minOrderPrice}원</Text>
+          </View>
         </TouchableOpacity>
       
   );
@@ -38,6 +44,20 @@ const styles = StyleSheet.create({
     padding : 12,
     borderBottomWidth: 1,
     borderColor: '#DFDFDF',
+    display : "flex",
+    flexDirection : "row"
+},
+leftBox:{ 
+  flex : 1,
+},
+rightBox:{
+  flex : 4,
+},
+imageStyle:{
+  width: 50,
+  height :50,
+  alignSelf : "center"
+
 },
 restaurantNameText:{
     fontSize : 20,
@@ -47,7 +67,6 @@ restaurantDetailText:{
   fontSize : 14,
   color: "gray",
 },
-
 
 
 });
