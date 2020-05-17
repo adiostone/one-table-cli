974be8905c1234665dca3b953790d0f505cd6ed3
wseungjin <bose1021@naver.com>
2020-05-17 21:45:18 +0900

    chore : apply event hook for go back button

--- src/screen/restaurantListScreen.js
+++ src/screen/restaurantListScreen.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState, useContext } from 'react'
 import { StyleSheet, Text, View,Button, Image,TextInput,TouchableOpacity,Dimensions, ScrollView, SafeAreaView } from 'react-native';
 import { AppContext } from '../context/AppContext'
 
-import RestuarantList from "../component/restaurantList"
-import LogoButton from "../component/logoButton"
+import BaseTab from "../component/baseTab"
 
+import * as SecureStore from 'expo-secure-store';
 import axios from 'axios'
 
-
+import RestuarantList from "../component/restaurantList"
 
 export default function restuarantListScreen({navigation}) {
 
@@ -17,10 +17,81 @@ export default function restuarantListScreen({navigation}) {
     const [category, setCategory] = useState("치킨") 
 
     useEffect(() => {
+      const unsubscribe = navigation.addListener('focus', () => {
+
+        axios({
+          method: 'get',
+          url: `https://dev.api.onetable.xyz/v1/table/restaurants?category=${category}`,
+          headers: {
+            Authorization: `Bearer ${appContext.accessToken}`,
+      }
+      })   
+      .then(res =>{
+          console.log("load restaurant list")
+          console.log(res.data)
+          //set restaurant list
+          setRestaurantList(res.data.restaurants)   
+
+      })
+      .catch(err =>{
+          if (err && err.response) {
+              console.log("failed load restaurant list")
+              console.log(err)
+              const status = err.response.status
+              if (status === 404) {
+                // Valid
+                console.log('valid tokens')
+              }
+              else{
+                console.log('invalid tokens -> refreshing tokens')
+                axios({
+                  url: 'https://dev.api.onetable.xyz/v1/table/auth/refresh',
+                  method: 'get',
+                  headers: {
+                    Authorization: `Bearer ${appContext.refreshToken}`,
+                  },
+                })
+                .then(res => {
+                  console.log('tokens have been refreshed')
+                  // Refresh the tokens and store to the machine again
+                  const { access } = res.data
+                  console.log(access)
+                  const accessToken= access    
+                  SecureStore.setItemAsync('accessToken', accessToken)
+                  appContext.setAccessToken(accessToken)
+                  axios({
+                      method: 'get',
+                      url: `https://dev.api.onetable.xyz/v1/table/restaurants?category=${category}`,
+                      headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                  }
+                  })   
+                  .then(res =>{
+                      console.log("load restaurant list")
+                      console.log(res.data)
+                      //set restaurant list
+                      setRestaurantList(res.data.restaurants)   
+                  })
+                })
+                // .catch(err =>{
+                //   console.log("could't refresh token")
+                // })
+              }
+            }
+      })
+
+
+      });
+    
+      return unsubscribe;
+    }, [navigation, category]);
+
+
+    useEffect(() => {
 
         axios({
             method: 'get',
-            url: `https://api.onetable.xyz/v1/table/restaurants?category=${category}`,
+            url: `https://dev.api.onetable.xyz/v1/table/restaurants?category=${category}`,
             headers: {
               Authorization: `Bearer ${appContext.accessToken}`,
         }
@@ -44,7 +115,7 @@ export default function restuarantListScreen({navigation}) {
                 else{
                   console.log('invalid tokens -> refreshing tokens')
                   axios({
-                    url: 'https://api.onetable.xyz/v1/table/auth/refresh',
+                    url: 'https://dev.api.onetable.xyz/v1/table/auth/refresh',
                     method: 'get',
                     headers: {
                       Authorization: `Bearer ${appContext.refreshToken}`,
@@ -60,7 +131,7 @@ export default function restuarantListScreen({navigation}) {
                     appContext.setAccessToken(accessToken)
                     axios({
                         method: 'get',
-                        url: `https://api.onetable.xyz/v1/table/restaurants?category=${category}`,
+                        url: `https://dev.api.onetable.xyz/v1/table/restaurants?category=${category}`,
                         headers: {
                           Authorization: `Bearer ${accessToken}`,
                     }
@@ -72,9 +143,9 @@ export default function restuarantListScreen({navigation}) {
                         setRestaurantList(res.data.restaurants)   
                     })
                   })
-                  .catch(err =>{
-                    console.log("could't refresh token")
-                  })
+                  // .catch(err =>{
+                  //   console.log("could't refresh token")
+                  // })
                 }
               }
         })
@@ -83,28 +154,28 @@ export default function restuarantListScreen({navigation}) {
 
     return (
         <SafeAreaView style={styles.container}>
-            <LogoButton/>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.tabButton} onPress={()=> setCategory("치킨")}>
-                    <Text style={styles.tabButtonText}>치킨</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("분식")}>
-                    <Text style={styles.tabButtonText}>분식</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("한식")}>
-                    <Text style={styles.tabButtonText}>한식</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("피자")}>
-                    <Text style={styles.tabButtonText}>피자</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("중식")}>
-                    <Text style={styles.tabButtonText}>중식</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("야식")}>
-                    <Text style={styles.tabButtonText}>야식</Text>
-                </TouchableOpacity>
-            </View>
-            <RestuarantList data={restaurantList}/>
+          <BaseTab/>
+          <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.tabButton} onPress={()=> setCategory("치킨")}>
+                  <Text style={styles.tabButtonText}>치킨</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("분식")}>
+                  <Text style={styles.tabButtonText}>분식</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("한식")}>
+                  <Text style={styles.tabButtonText}>한식</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("피자")}>
+                  <Text style={styles.tabButtonText}>피자</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("중식")}>
+                  <Text style={styles.tabButtonText}>중식</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.tabButton} onPress={()=>  setCategory("야식")}>
+                  <Text style={styles.tabButtonText}>야식</Text>
+              </TouchableOpacity>
+          </View>
+          <RestuarantList data={restaurantList}/>
         </SafeAreaView>
 
         
