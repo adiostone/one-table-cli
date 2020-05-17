d21d19230d393c306a73eeb7ca0643cc35c75edb
wseungjin <bose1021@naver.com>
2020-05-18 02:49:19 +0900

    feat : apply new UI for partyItem

--- src/component/partyItem.js
+++ src/component/partyItem.js
@@ -14,8 +14,9 @@ export default function partyItem(props) {
 
   const ws = props.ws;
 
-  const partyID = props.data.id 
-  const restaurant = props.data.restaurant 
+  const id = props.data.id 
+ 
+  const restaurant = props.data.restaurant
   const address = props.data.address 
   const capacity = props.data.capacity
   const size = props.data.size
@@ -30,20 +31,24 @@ export default function partyItem(props) {
   function joinParty(){
     if (!ws.current) return;
 
-    const message = { operation: 'joinParty', body: {id : partyID } }
+    const message = { operation: 'joinParty', body: {id : id } }
     ws.current.send(JSON.stringify(message))
   }
 
   return (
-
-          <TouchableOpacity style={styles.listBox} onPress={joinParty}>
-            <Text style={styles.restaurantNameText}> {restaurant.name}</Text>
-            <Text style={styles.addressText}>{address}</Text>
-            <Text style={styles.partyNameText}>{partyName}</Text>
-            <Text style={styles.peopleNumberText}>{capacity}</Text>
-            <Text style={styles.peopleNumberText}> {size}/{capacity}</Text>
-          </TouchableOpacity>        
-      
+            <TouchableOpacity style={styles.listBox} onPress={joinParty}>
+            <View style={styles.leftBox}>
+              <Image source={{uri:(restaurant===undefined)? restaurant:restaurant.icon}} style={styles.imageStyle}/>
+            </View>
+            <View style={styles.rightBox}>
+              <Text style={styles.restaurantNameText}> {(restaurant===undefined)? restaurant:restaurant.name}</Text>
+              <Text style={styles.addressText}>{address}</Text>
+              <View style={styles.rightBottomBox}>
+                <Text style={styles.partyNameText}>{partyName}</Text>
+                <Text style={styles.peopleNumberText}> {size}/{capacity}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>       
   );
 }
 
@@ -60,36 +65,63 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
 
   },
+  imageStyle:{
+    width: 100,
+    height :100,
+    alignSelf : "center"
+
+  },
   listBox:{
     width: 335,
-    height: 80,
+    height: 100,
 
     backgroundColor: "#CB661D",
     borderRadius: 10,
     marginBottom : 12, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
+    display : "flex",
+    flexDirection : "row",
+    shadowColor : '#4d4d4d',
+    shadowOffset: { width: 8, height: 8, },
+    shadowOpacity: 0.3, 
+    shadowRadius: 4,
+  },
+  leftBox:{ 
+    flex : 1,
+    backgroundColor: '#fff',
 
+
+  },
+  rightBox:{
+    flex : 2,
+    padding : 8,
+
+  },
+  rightBottomBox:{
+    display : "flex", 
+    flexDirection : "row"
   },
   restaurantNameText:{
     fontStyle: 'normal',
     fontSize: 18,
     fontWeight : "bold",
     color: "#FFFFFF",
+    marginBottom: 5,
   },
   addressText:{
     fontStyle: 'normal',
-    fontSize: 15,
+    fontSize: 12,
     color: "#FFFFFF",
+    marginBottom: 3,
   },
   partyNameText:{
     fontStyle: 'normal',
-    fontSize: 12,
+    fontSize: 18,
     color: "#FFFFFF",
+    marginRight : 10,
   },
   peopleNumberText:{
     fontStyle: 'normal',
-    fontSize: 12,
+    fontSize: 18,
     color: "#FFFFFF",
   }
 
