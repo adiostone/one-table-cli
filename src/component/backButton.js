eab0dcbd457c30f7b0c60173abbe37492eab919e
wseungjin <bose1021@naver.com>
2020-05-17 21:41:08 +0900

    feat : add backButton

--- src/component/backButton.js
+++ src/component/backButton.js
@@ -1,43 +1,31 @@
 import React, { useEffect, useState, useContext , useRef} from 'react'
 import { StyleSheet, Text, View,Button, Image,TextInput,Dimensions ,ScrollView,TouchableOpacity,SafeAreaView} from 'react-native';
 import { AppContext } from '../context/AppContext'
-
-
 import { useNavigation } from '@react-navigation/native';
 
-
-export default function backButton(props) {
+export default function backButton() {
 
   const navigation = useNavigation();
-  const information = props.information
-  const backLocation = props.backLocation
 
   return (
+        <TouchableOpacity  onPress={() => navigation.goBack()}>
+                <Image source={require('../assets/backButton.png')} style={styles.backStyle} />
+        </TouchableOpacity>            
 
-    <TouchableOpacity style={styles.backBox} onPress={() => navigation.navigate(backLocation)}>
-        <Text style={styles.backText}>{information}</Text>
-    </TouchableOpacity>             
-      
   );
 }
 
 const styles = StyleSheet.create({
 
-    backBox:{
-        width: 335,
-        height: 39,
-        backgroundColor: "#FF7E47",
-        borderRadius: 10,
-        marginBottom : 12, 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        alignSelf : "center",
-      },
-      backText:{
-        fontStyle: 'normal',
-        fontSize: 20,
-        textAlign: "center",
-        color: "#FFFFFF",
-      },
+  backStyle: {
+    width: 40,
+    height: 30,
+    marginLeft:20,
+    marginTop:20,
+    marginBottom:13,
+
+  },
+
+
 
 });
