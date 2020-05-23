8ff79b5cd5e7a23068af20be67115de410f23ba8
wseungjin <bose1021@naver.com>
2020-05-23 22:01:56 +0900

    feat : apply websocket on shopping bagList

--- src/screen/shoppingBagScreen.js
+++ src/screen/shoppingBagScreen.js
@@ -1,15 +1,61 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState, useContext , useRef } from 'react'
 import { StyleSheet, Text, View,Button, Image,TextInput,TouchableOpacity,Dimensions, ScrollView, SafeAreaView } from 'react-native';
 import BagFoodList from "../component/bagFoodList"
 import BaseTab from "../component/baseTab"
 import { AppContext } from '../context/AppContext'
+import { SocketContext } from '../context/SocketContext'
 
-
-export default function shoppingBagScreen({route,navigation}) {
+export default function shoppingBagScreen({navigation}) {
 
   const appContext = useContext(AppContext)
 
-  const [bagFoodList , setBagFoodList] = useState()
+  const socketContext = useContext(SocketContext)
+
+  const ws = useRef(socketContext.ws)
+
+  const [bagFoodList , setBagFoodList] = useState(appContext.bagFoodList)
+
+  useEffect(() => {
+    if (!ws.current || ws.current.readyState === WebSocket.CLOSED) return;
+
+    ws.current.onmessage = e => {
+        const message = JSON.parse(e.data);
+        console.log("shopping bag listen")
+        console.log(message);
+        if(message.operation==="replyUpdateShoppingBag"){
+          for (let i=0 ; i <bagFoodList.length; i++){
+            if(bagFoodList[i].id===message.body.id){
+              bagFoodList[i] = message.body
+              SetBagFoodList([...bagFoodList])
+            } 
+          } 
+          for (let i=0 ; i <appContext.bagFoodList.length; i++){
+            if(appContext.bagFoodList[i].id===message.body.id){
+              appContext.bagFoodList[i] = message.body
+              SetBagFoodList([...appContext.bagFoodList])
+            } 
+          } 
+        }
+        if(message.operation==="replyDeleteShoppingBag"){
+          for (let i=0 ; i <bagFoodList.length; i++){
+            if(bagFoodList[i].id===message.body.id){
+              bagFoodList.splice(i,1)
+              SetBagFoodList([...bagFoodList])
+            } 
+          } 
+          for (let i=0 ; i <appContext.bagFoodList.length; i++){
+            if(appContext.bagFoodList[i].id===message.body.id){
+              appContext.bagFoodList.splice(i,1)
+              SetBagFoodList([...appContext.bagFoodList])
+            } 
+          } 
+        }
+        if(message.operation==="ping"){
+          const sendMessage = { operation: 'pong'}
+          ws.current.send(JSON.stringify(sendMessage))
+        }
+    };
+  });
 
     return (
         <SafeAreaView style={styles.container}>
