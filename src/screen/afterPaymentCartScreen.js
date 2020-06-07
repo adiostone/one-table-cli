3193ba4e2d595ed9d2475d2aff887644e04b3729
wseungjin <bose1021@naver.com>
2020-06-07 15:43:31 +0900

    feat: add afterPaymentCartScreen  UI and view cart

---
+++ src/screen/afterPaymentCartScreen.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState, useContext , useRef } from 'react'
+import { StyleSheet, Text, View,Button, Image,TextInput,TouchableOpacity,Dimensions, ScrollView, SafeAreaView } from 'react-native';
+import ViewCartList from "../component/viewCartList"
+import LogoButton from "../component/logoButton"
+import BaseTab from "../component/baseTab"
+import { AppContext } from '../context/AppContext'
+import { SocketContext } from '../context/SocketContext'
+
+export default function afterPaymentCartScreen({navigation}) {
+
+  const appContext = useContext(AppContext)
+
+  const socketContext = useContext(SocketContext)
+
+  const ws = useRef(socketContext.ws)
+
+  const [cartList , setCartList] = useState(appContext.cartList)
+
+  useEffect(() => {
+    if (!ws.current || ws.current.readyState === WebSocket.CLOSED) return;
+
+    ws.current.onmessage = e => {
+        const message = JSON.parse(e.data);
+        console.log("afterPaymentCart listen")
+        console.log(message);
+        if(message.operation==="ping"){
+          const sendMessage = { operation: 'pong'}
+          ws.current.send(JSON.stringify(sendMessage))
+        }
+    };
+  });
+
+    return (
+        <SafeAreaView style={styles.container}>
+          <BaseTab data={"afterPaymentChat"}/>
+          <ScrollView style={styles.scrollBox}>
+            <View style={styles.cartBox}>
+                <Text style={styles.cartText}>최종 주문 내역</Text>
+            </View>
+            <ViewCartList data={cartList}/>
+          </ScrollView>
+        </SafeAreaView>
+
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flex :1 ,
+
+      },
+      cartBox:{
+        width: 340,
+        height: 39,
+        backgroundColor: "#FF8181",
+        borderRadius: 10,
+        marginBottom : 12, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        alignSelf:"center"
+    
+      },
+      cartText:{
+        fontStyle: 'normal',
+        fontSize: 20,
+        textAlign: "center",
+        color: "#FFFFFF",
+    
+      },
+  
+
+  });
+  
