84efc7ddc03ecc5823eed40cb18f55baa1cd3b28
wseungjin <bose1021@naver.com>
2020-06-13 23:02:13 +0900

    feat: nofify who is paid and accept , refuse

--- src/screen/paymentScreen.js
+++ src/screen/paymentScreen.js
@@ -11,13 +11,67 @@ export function paymentScreen({route ,navigation  }) {
 
     const appContext = useContext(AppContext)
 
+    const socketContext = useContext(SocketContext)
+
+    const ws = useRef(socketContext.ws)
+
+    useEffect(() => {
+        if (!ws.current) return;
+    
+        ws.current.onmessage = e => {
+            const message = JSON.parse(e.data);
+            console.log("payment listen")
+            console.log(message);
+            if(message.operation==="replyVerifyPayment"){
+              if(message.body.isSuccess ===true){
+                for(let i=0 ; i<message.body.isPaidList.length ; i++ ){
+                    for(let j=0 ; j<appContext.userList.length ; j++ ){
+                        if(message.body.isPaidList[i].id===appContext.userList[j].id){
+                            appContext.userList[j]["isPaid"]=message.body.isPaidList[i].isPaid
+                            appContext.setUserList([...appContext.userList])
+                        }
+                    }
+                }
+                navigation.replace('paymentResult');
+                }
+            }
+            if(message.operation==="notifyCompletePayment"){
+                for(let i=0 ; i<appContext.userList.length ; i++ ){
+                    if(message.body.id===appContext.userList[i].id){
+                        appContext.userList[i].isPaid=true
+                        appContext.setUserList([...appContext.userList])
+                    }
+                }
+            }
+            if(message.operation==="notifyOrderIsAccepted"){
+            Alert.alert("주문이 접수되었습니다")
+            }
+            if(message.operation==="notifyOrderIsRefused"){
+            Alert.alert("주문이 거절되었습니다")
+            }
+            if(message.operation==="notifyStartDelivery"){
+            Alert.alert("주문이 배달 시작하였습니다")
+            }
+        if(message.operation==="ping"){
+              const sendMessage = { operation: 'pong'}
+              ws.current.send(JSON.stringify(sendMessage))
+            }
+        };
+      });
+    
+      function verifyPayment(response){
+        if (!ws.current) return;
+        const message = { operation: 'verifyPayment', body: {impUID: response.imp_uid,merchantUID : response.merchant_uid} }
+        ws.current.send(JSON.stringify(message))
+      }
     /* [필수입력] 결제 종료 후, 라우터를 변경하고 결과를 전달합니다. */
     function callback(response) {
         console.log(response)
         if(response.imp_success==="true"){
-            navigation.replace('paymentResult', response);
+            verifyPayment(response)
         }
         else{
+            //payment failed
             navigation.navigate('confirmOrder');
         }
     }
@@ -28,8 +82,8 @@ export function paymentScreen({route ,navigation  }) {
         pay_method: 'card',
         name: '일인식탁',
         merchant_uid: `mid_${new Date().getTime()}`,
-        // amount: route.params.finalPrice,
-        amount: 100,
+        amount: route.params.finalPrice,
+        // amount: 100,
         buyer_name: appContext.nickName,
         buyer_tel: route.params.phoneNum,
         buyer_email: appContext.nickName +'@gmail.com',
