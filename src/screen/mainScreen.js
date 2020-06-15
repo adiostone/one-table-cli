9d41b6e5edaeddbb3d99bda147f21e76b5673138
wseungjin <bose1021@naver.com>
2020-06-15 12:45:45 +0900

    feat : add hungry mode for getting push notifi

--- src/screen/mainScreen.js
+++ src/screen/mainScreen.js
@@ -19,6 +19,8 @@ export default function mainScreen({navigation}) {
 
   const [partyList, setPartyList]= useState([]) 
 
+  const [isHungry, setIsHungry] = useState(appContext.isHungry)
+
   const ws = useRef(socketContext.ws)
 
   useEffect(() => {
@@ -34,9 +36,9 @@ export default function mainScreen({navigation}) {
     appContext.setWholePrice(0)
     appContext.setIsDelivered(false)
     appContext.setIsEnter(false)
-    appContext.setCartList([])
-    appContext.setFinalCart([])
-    appContext.setUserList([])
+    // appContext.setCartList([])
+    // appContext.setFinalCart([])
+    // appContext.setUserList([])
 
     if (ws.current && ws.current.readyState === WebSocket.OPEN){
       console.log("getPartyList")
@@ -106,17 +108,56 @@ export default function mainScreen({navigation}) {
       };
   });
 
+  async function setHungry(){
+    if(isHungry===true){
+      const res = await axios({
+        method: 'patch',
+        url: 'https://api.onetable.xyz/v1/table/me/profile',
+        headers: {
+          Authorization: `Bearer ${appContext.accessToken}`,
+        },
+        data :  {
+          "isHungry" : false
+        }
+      })
+      setIsHungry(false)
+      appContext.setIsHungry(false)
+
+    }
+    else{
+      const res = await axios({
+        method: 'patch',
+        url: 'https://api.onetable.xyz/v1/table/me/profile',
+        headers: {
+          Authorization: `Bearer ${appContext.accessToken}`,
+        },
+        data :  {
+          "isHungry" : true
+        }
+      })
+      setIsHungry(true)
+      appContext.setIsHungry(true)
+    }
+  }
+
 
   return (
     <SafeAreaView style={styles.container}>
         <View style={styles.logo}>
           <LogoButton/>
         </View>
+        {(isHungry===true)?
+        <TouchableOpacity style={styles.hungryBox} onPress={setHungry}>
+            <Text style={styles.hungryText}>배고파 모드</Text>
+        </TouchableOpacity> :<TouchableOpacity style={styles.notHungryBox} onPress={setHungry}>
+            <Text style={styles.notHungryText}>배고파 모드</Text>
+        </TouchableOpacity> 
+        }
         <ScrollView style={styles.pinContainer}>
-          <TouchableOpacity style={styles.locationBox} onPress={() => navigation.navigate('map')}>
+          <TouchableOpacity style={styles.locationBox} onPress={() => navigation.replace('map')}>
             <Text style={styles.locationText}>위치 설정</Text>
           </TouchableOpacity>        
-          <TouchableOpacity style={styles.createBox} onPress={() => navigation.navigate('restaurantList')}>
+          <TouchableOpacity style={styles.createBox} onPress={() => navigation.replace('restaurantList')}>
             <Text style={styles.createText}>파티 만들기</Text>
           </TouchableOpacity>
           <PartyList data={partyList}/>
@@ -141,6 +182,43 @@ const styles = StyleSheet.create({
   },
   logo:{
     alignItems: "center",
+  },
+  hungryBox:{
+    backgroundColor : "#83FF81",
+    borderRadius : 5,
+    marginRight :20,
+    marginTop : 20,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    padding : 5
+
+  },
+  hungryText:{
+    fontStyle: 'normal',
+    fontSize: 10,
+    textAlign: "center",
+    fontWeight : "bold",
+
+  },
+  notHungryBox:{
+    backgroundColor : "#E53030",
+    borderRadius : 5,
+    marginRight :20,
+    marginTop : 20,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    padding : 5
+
+  },
+  notHungryText:{
+    fontStyle: 'normal',
+    fontSize: 10,
+    textAlign: "center",
+    fontWeight : "bold",
+    color :"#fff",
+
   },
   locationBox:{
     height: 39,
